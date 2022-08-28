9fc3a7fc3c06da8d0b2fbd875e937fc9fee5d896
johan-rain <johan@ranestam.se>
2022-08-28 09:57:51 +0200

    fixed the navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,16 +6,21 @@ import { Link, NavLink } from 'react-router-dom'
 const Navigation = () => {
 	return (
 		<Navbar bg="dark" variant="dark" expand="md">
+
 			<Container>
-				<Navbar.Brand as={Link} to="/">The Movie DB</Navbar.Brand>
+				<Navbar.Brand as={Link} to="/">MovieDB</Navbar.Brand>
 
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="ms-auto">
-						<Nav.Link as={NavLink} end to="/">Home</Nav.Link>
+						<Nav.Link as={NavLink} end to="/current">Current Movies</Nav.Link>
+						<Nav.Link as={NavLink} end to="/popular">Popular Movies</Nav.Link>
+						<Nav.Link as={NavLink} end to="/toplist">Toplist</Nav.Link>
+						<Nav.Link as={NavLink} end to="/genres">Genres</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
+			
 		</Navbar>
 	)
 }
