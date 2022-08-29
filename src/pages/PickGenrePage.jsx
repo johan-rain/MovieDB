2246fff818e6d515408e9df315b2cd810bf92339
johan-rain <johan@ranestam.se>
2022-08-29 23:10:59 +0200

    finshed with pagination and the styling of the app, finally

--- src/pages/PickGenrePage.jsx
+++ src/pages/PickGenrePage.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import { useParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import useFilmGenre from "../hooks/useFilmGenre"
 import FilmCard from "../components/FilmCard"
-import { useSearchParams } from "react-router-dom";
+import Pagination from "../components/Pagination"
 import LoadingSpinner from "../components/LoadingSpinner";
 
 const PickGenrePage = () => {
 	const [searchParams, setSearchParams] = useSearchParams({ page: 1 });
-	const page = searchParams.get("page")
+	const page = searchParams.get("page") ? Number(searchParams.get("page")) : null;
 	const { id, name } = useParams();
 	const { data: movies, isLoading } = useFilmGenre(id, page);
 
@@ -31,6 +32,17 @@ const PickGenrePage = () => {
 							</Col>
 						))}
 					</Row>
+
+					<Pagination
+						page={page}
+						numPages={Math.ceil(movies.total_pages)}
+						hasPreviousPage={movies.page !== 1}
+						hasNextPage={movies.page !== movies.total_pages}
+						onPreviousPage={() =>
+							setSearchParams({ page: page - 1 })
+						}
+						onNextPage={() => setSearchParams({ page: page + 1 })}
+					/>
 				</div>
 			)}
 		</Container>
