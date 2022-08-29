f53e24ab370a5eca5c30ac0dce0a2b67c414b484
johan-rain <johan@ranestam.se>
2022-08-29 20:59:39 +0200

    started on genres page

--- src/pages/GenresPage.jsx
+++ src/pages/GenresPage.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import useGenres from "../hooks/useGenres";
+import LoadingSpinner from "../components/LoadingSpinner";
 
-function GenresPage() {
+const GenresPage = () => {
+	const { data: genres, isLoading } = useGenres();
+
 	return (
-		<div>
-			
-		</div>
-	)
-}
+		<Container className="py-3">
 
-export default GenresPage
+			{isLoading && <LoadingSpinner />}
+
+			{genres && (
+				<div className="white-container ">
+
+					<h2 className="mb-5 d-flex justify-content-center">Genres</h2>
+
+				</div>
+			)}
+			
+		</Container>
+	);
+};
+
+export default GenresPage;
