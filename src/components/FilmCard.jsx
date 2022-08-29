7d7b63c1a03cd09a7ec5ccf97d5e3003b8ffb55c
johan-rain <johan@ranestam.se>
2022-08-29 17:42:16 +0200

    Homepage fixed with old code and Filmcard component

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Card from "react-bootstrap/Card";
+import LoadingSpinner from "./LoadingSpinner";
 
-function FilmCard() {
+const MovieCard = ({ movie }) => {
+	const [poster, setPoster] = useState("");
+	const [loading, setLoading] = useState("true");
+
+	useEffect(() => {
+		setTimeout(() => {
+			if (!movie.poster_path) {
+				setPoster(
+					"https://via.placeholder.com/300x450?text=Poster+Not+Found"
+				);
+			} else {
+				setPoster(
+					`https://image.tmdb.org/t/p/w300${movie.poster_path}`
+				);
+			}
+
+			setLoading(false);
+		}, 1000);
+	}, []);
+
 	return (
-		<div>
-			
-		</div>
-	)
-}
+		<>
+			{loading && <LoadingSpinner />}
 
-export default FilmCard
+			{poster && (
+
+				<Link to={`/movie/${movie.id}`}>
+					
+					<Card border="dark" className="bg-light movie-card">
+						<img className=" movie-img" src={`${poster}`} />
+					</Card>
+
+					<p className="film-text">{movie.title}</p>
+
+				</Link>
+			)}
+		</>
+	);
+};
+
+export default MovieCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,71 @@
-import Container from 'react-bootstrap/Container'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import FilmCard from "../components/FilmCard"
+import usePlayingNow from "../hooks/usePlayingNow"
+import useTopRated from "../hooks/useTopRated";
+import usePopularFilms from "../hooks/usePopularFilms"
+import LoadingSpinner from "../components/LoadingSpinner";
 
 const HomePage = () => {
-	return (
-		<Container className="py-3">
-			<h1>Welcome!</h1>
-		</Container>
-	)
-}
+	const { data: popularMovies, isError, isLoading } = usePlayingNow();
+	const { data: nowPlaying } = usePopularFilms();
+	const { data: topRated } = useTopRated();
 
-export default HomePage
+	return (
+		<Container fluid="lg" className="py-3">
+
+			{isError && <p>Something is wrong</p>}
+
+			{isLoading && <LoadingSpinner />}
+
+			{popularMovies && nowPlaying && topRated && (
+				<>
+					<div className="white-container">
+
+						<h2 className="mb-5 d-flex justify-content-center">Popular Movies</h2>
+
+						<Row>
+							{popularMovies.results.map((movie) => (
+								<Col lg={3} md={4} sm={10} key={movie.id}className="pb-5">
+									<FilmCard movie={movie} />
+								</Col>
+							))}
+						</Row>
+
+					</div>
+
+					<div className="white-container">
+
+						<h2 className="mb-5 d-flex justify-content-center">Playing Now</h2>
+
+						<Row>
+							{nowPlaying.results.map((movie) => (
+								<Col lg={3} md={4} sm={10} key={movie.id} className="pb-5">
+									<FilmCard movie={movie} />
+								</Col>
+							))}
+						</Row>
+						
+					</div>
+
+					<div className="white-container">
+						
+						<h2 className="mb-5 d-flex justify-content-center">Top Rated</h2>
+						
+						<Row>
+							{topRated.results.map((movie) => (
+								<Col lg={3} md={4} sm={10} key={movie.id} className="pb-5">
+									<FilmCard movie={movie} />
+								</Col>
+							))}
+						</Row>
+
+					</div>
+				</>
+			)}
+		</Container>
+	);
+};
+
+export default HomePage;
